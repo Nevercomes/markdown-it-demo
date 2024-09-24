8291c4bfb0e677174357b5d0a27b3baa45004bcc
Jiacheng Sun <jiachengs@moqi.ai>
2024-09-24 17:14:44 +0800

    use dollars and brackets delimiters

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Markdown + TeXMath Demo</h1>
+    <textarea v-model="input" placeholder="Type markdown with TeXmath here..." />
+    <div v-html="renderedMarkdown" class="markdown-content"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkdownIt from "markdown-it";
+import texmath from "markdown-it-texmath";
+import "katex/dist/katex.min.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      /* eslint-disable no-useless-escape */
+      input: String.raw`### Dollar
+$a^2+b^2=c^2$
+
+$$a^2+b^2=c^2$$
+
+$$
+a^2+b^2=c^2
+$$
+
+$$\text{Cl}_2 + \text{S}^{2-} = \text{S}↓ + 2\text{Cl}^-$$
+
+
+### Bracket
+
+\(a^2+b^2=c^2\)
+
+\[a^2+b^2=c^2\]
+
+\[
+a^2+b^2=c^2
+\]
+
+\[\text{Cl}_2 + \text{S}^{2-} = \text{S}↓ + 2\text{Cl}^-\]
+`,
+      md: null,
+    };
+  },
+  computed: {
+    renderedMarkdown() {
+      return this.md.render(this.input);
+    },
+  },
+  created() {
+    this.md = new MarkdownIt().use(texmath, {
+      engine: require("katex"),
+      delimiters: ["dollars", "brackets"],
+    });
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.markdown-content {
+  border: 1px solid #ddd;
+  padding: 1em;
+  margin-top: 1em;
+}
+textarea {
+  width: 100%;
+  height: 150px;
+  margin-bottom: 1em;
 }
 </style>
